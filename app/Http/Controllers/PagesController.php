575197e9f2018e9bc5fbb0741968bd0da7888db0
tejas100 <tejas55bk@gmail.com>
2019-03-01 19:14:23 +0530

    first commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+$title = "wecom";
+// return view('pages.index', compact('title') );
+return view('pages.index')->with('title',$title);
+
+    }
+
+    public function about(){
+        return view ('pages.about');
+    }
+
+    public function services(){
+        $data = array(
+
+            'title'=>'Service',
+            'servlist'=>['web','android','ML']
+        );
+        return view ('pages.services')->with($data);
+    }
+}
